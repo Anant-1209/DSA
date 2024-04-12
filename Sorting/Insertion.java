9ef49c01087d40516c09c3c9692ffde965aea7b6
Anant <a2000anantsahu@gmail.com>
2024-04-12 10:04:14 +0530

    sorting

---
+++ Sorting/Insertion.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Insertion {
+
+    public static void insertionSort(int arr[], int n) {
+        int i, j, key;
+        for (i = 1; i < n; i++) {
+            key = arr[i];
+            j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
